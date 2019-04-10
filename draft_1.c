73baee9ca0b2f94716b52f05f3e6cc3773165abf
jackson boustany <gravity267@hotmail.com>
2019-04-10 00:48:15 +0000

    modified online templet i found, needs improvement

--- draft_1.c
+++ draft_1.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
-void encryptSentence();
 
 int main()
 {
-    printf("Enter a sentence: ");
-    encryptSentence();
+   int i, x;
+   char str[26]; 
 
-    return 0;
+   printf("\nPlease enter a string:\t");
+   gets(str);
+
+   printf("\nPlease choose following options:\n");
+   printf("1 = Encrypt the string.\n");
+   printf("2 = Decrypt the string.\n");
+   printf("Choice:\t");
+   scanf("%d", &x);
+
+   //can i remove the switch statement to just encrypt and decrypt in one go?
+   switch(x)
+   {
+   case 1:
+      for(i = 0; (i < 26 && str[i] != '\0'); i++)
+        str[i] = str[i] + 9; //shifted by 9
+
+      printf("\nEncrypted string: %s\n", str);
+      break;
+
+   case 2:
+      for(i = 0; (i < 26 && str[i] != '\0'); i++)
+        str[i] = str[i] - 9; //shifted back to original by 9
+
+      printf("\nDecrypted string: %s\n", str);
+      break;
+
+   default:
+      printf("\nError\n");
+   }
+   return 0;
 }
-
-void encryptSentence()
-{
-    char c;
-    scanf("%c", &c);
-
-    if( c != '\n')
-    {
-        encryptSentence();
-        printf("%c",c);
-    }
-}  // function to reverse a sentence
